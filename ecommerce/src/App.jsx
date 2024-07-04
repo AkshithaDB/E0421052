a54d84753566a4a7d8cb95ce6591f0f1cc0787e3
Akshitha <3gendatasystemsprivatelimited@3Gens-MacBook-Air.local>
2024-07-04 14:48:35 +0530

    initial commit

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Mylaptops from './laptops'
+
+function App() {
+  
+
+  return (
+    <>
+     <h3>E-commerce website</h3>
+     <header className='header'>
+      <h2> top 10 laptops</h2>
+      <button> Laptop Price</button>
+     
+
+     </header>
+
+    </>
+  )
+}
+
+export default App
